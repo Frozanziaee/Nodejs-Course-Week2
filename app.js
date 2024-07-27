f564ce1ae07e0941caa184c0593c690af6c19c8f
Frozanziaee <130326629+Frozanziaee@users.noreply.github.com>
2024-07-28 02:20:49 +0430

    add studensjs

--- app.js
+++ app.js
@@ -1,33 +1,39 @@
 const express = require('express')
 const app = express()
+const {students} = require('./data')
 
-const { instructors, courses, students } = require("./data")
+app.get('/', (req, res) => res.status(200).json({students}))
 
+app.get('/students', (req, res) =>{
+  const {age} = req.query
+  let filteredStudents = students
 
-// app.get("/courses", (req, res) => {
-//   res.status(200).json(courses)
-// })
-
-app.get("/courses", (req, res) => {
-  console.log (req.query)
-  const tagsFilter = req.query.tags ? req.query.tags.split(",") : null
-  const filteredcourses = []
-
-  courses.map((course) => {
-    if (tagsFilter) {
-      for (let tags of tagsFilter) {
-        if (course.tags.includes(tags)) {
-          filteredcourses.push(course)
-        }
-      }
-    } else {
-      filteredcourses.push(course)
-    }
-  })
-  res.status(200).json({ courses: filteredcourses })
+  if(age){
+    const [minAge, maxAge] =age.split('-')
+    filteredStudents = students.filter(
+      (student) => student.age >= minAge && student.age <= maxAge
+    )
+  }
+  res.status(200).json({students: filteredStudents})
 })
 
+app.get("/students/:id", (req, res) => {
+  const { id } = req.params
+  let student = students.find((student) => student.student_id == id)
+  if (student) {
+    return res.status(200).json({ student })
+  }
+  res.status(402).json({ message: `invalid student id ${id}` })
+})
+
+app.get("/students/:id/majors/:major([a-zA-Z0-9%20]+)", (req, res) => {
+  const { major } = req.params
+  let student = students.filter(
+    (student) => student.major.toLowerCase() == major.toLowerCase()
+  )
+  return res.status(200).json({ student })
+})
 
 app.listen(3000, () => {
-  console.log("Server is lestening on port 3000...")
+  console.log('Server is listening on port 3000....')
 })
---
+++ courses.js
@@ -0,0 +1,73 @@
+const express = require('express')
+const app = express()
+
+const { instructors, courses, students } = require("./data")
+
+
+app.get("/", (req, res) => {
+  res.status(200).json(courses)
+})
+
+app.get("/courses", (req, res) => {
+    const tagsFilter = req.query.tags ? req.query.tags.split(",") : null;
+    const filteredcourses = [];
+  
+    for (let course of courses) {
+      if (tagsFilter) {
+        for (let tags of tagsFilter) {
+          let tagMatches = false;
+          course.tags.map((tag) => {
+            if (tag.toLowerCase() === tags.toLowerCase()) {
+              tagMatches = true;
+            }
+          })
+          if (tagMatches) {
+            filteredcourses.push(course)
+          }
+        }
+      } else {
+        filteredcourses.push(course)
+      }
+    }
+    res.status(200).json({ courses: filteredcourses })
+  })
+
+app.get("/courses/:id", (req, res) => {
+  const { id } = req.params
+  const course = courses.find((course) => course.course_id == id)
+
+  if (course) {
+    return res.status(200).json({ course })
+  }
+  return res.status(404).json({ message: `invalid course id ${id}` })
+})
+
+app.get("/courses/:id/instructors", (req, res) => {
+  const { id } = req.params
+  const course = courses.find(course => course.course_id == id)
+
+  if (course) {
+    const courseInstructors = instructors.filter((instr) =>
+      course.instructors.includes(instr.name)
+    )
+    return res.status(200).json({ instructors: courseInstructors })
+  }
+  return res.status(404).json({ message: `invalid course id ${id}` })
+})
+
+app.get("/courses/:id/students", (req, res) => {
+  const { id } = req.params;
+  const course = courses.find((course) => course.course_id == id)
+
+  if (course) {
+    const courseStudents = students.filter((student) =>
+      student.courses.includes(course.course_name)
+    )
+    return res.status(200).json({ students: courseStudents })
+  }
+  return res.status(404).json({ message: `invalid course id ${id}` })
+})
+
+app.listen(3000, () => {
+  console.log("Server is lestening on port 3000...")
+})
